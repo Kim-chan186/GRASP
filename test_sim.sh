48fae81d048a40659f6c78ff4430ac253a2fc108
Kim-chan186 <kch1051104@naver.com>
2024-08-21 21:22:12 +0900

    Create test_sim.sh

---
+++ test_sim.sh
@@ -0,0 +1,23 @@
+CUDA_VISIBLE_DEVICES=0 python test_graspnet.py \
+--center-num 48 \
+--anchor-num 7 \
+--anchor-k 6 \
+--anchor-w 50 \
+--anchor-z 20 \
+--grid-size 8 \
+--scene-l 130 \
+--scene-r 160 \
+--all-points-num 25600 \
+--group-num 512 \
+--local-k 10 \
+--ratio 8 \
+--input-h 360 \
+--input-w 640 \
+--local-thres 0.01 \
+--heatmap-thres 0.01 \
+--num-workers 4 \
+--dataset-path '/home/chkim/GRASP/hggd_data/realsense/6dto2drefine_realsense' \
+--checkpoint './model' \
+--scene-path '/home/chkim/GRASP/GraspNet1Billiion/scene_data' \
+--dump-dir 'pred_grasps' \
+--description 'realsense_similar'
